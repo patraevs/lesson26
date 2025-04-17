a4503c4450638b4de9df796a3c15cc5f8face22e
patraevs <nvnesh@yandex.ru>
2025-04-17 17:01:28 +0300

    Promise. Fetch.

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-console.log('test');
+const getData = () => {
 
+    fetch('db.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            sendData(data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+const sendData = (data) => {
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+getData()
